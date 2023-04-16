f8d28d03a621ecc72891878060602282047560fd
volsvik <rudyard47@gmail.com>
2023-04-17 00:18:30 +0300

    feat: task work

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
@@ -11,38 +11,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
- function getSeason(/*date*/) {
-  throw new NotImplementedError('Not implemented');
-//   let rez = ''
-// if (date === undefined) {
-//   rez = 'Unable to determine the time of year!'
-// } else {
-//   if (date instanceof Date) {
-//     if (Object.prototype.toString.call(date) === 'Date') {
-//       throw Error("Invalid date!")
-//     } else {
-//       if (date.getMonth() > 10 || date.getMonth() < 2) {
-//         rez = 'winter'
-//       } else {
-//       if (date.getMonth() >= 2 && date.getMonth() < 5) {
-//         rez = 'spring'
-//       } else {
-//         if (date.getMonth() >= 5 && date.getMonth() < 8) {
-//           rez = 'summer'
-//         } else {
-//           rez = 'autumn'
-//         }
-//       }
-//     }
+ function getSeason(date) {
+  // throw new NotImplementedError('Not implemented');
+  let rez = ''
+if (date === undefined) {
+  rez = 'Unable to determine the time of year!'
+} else {
+  if (date instanceof Date) {
+    if (Object.prototype.toString.call(date) === 'Date') {
+      throw Error("Invalid date!")
+    } else {
+      if (date.getMonth() > 10 || date.getMonth() < 2) {
+        rez = 'winter'
+      } else {
+      if (date.getMonth() >= 2 && date.getMonth() < 5) {
+        rez = 'spring'
+      } else {
+        if (date.getMonth() >= 5 && date.getMonth() < 8) {
+          rez = 'summer'
+        } else {
+          rez = 'autumn'
+        }
+      }
+    }
 
-//     }
+    }
 
-//   } else {
-//     throw Error("Invalid date!")
-//     // return 'Invalid date!'
-//   }
-// }
-// return rez
+  } else {
+    throw Error("Invalid date!")
+    // return 'Invalid date!'
+  }
+}
+return rez
 }
 
 module.exports = {
